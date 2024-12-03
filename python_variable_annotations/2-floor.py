55cef0039db194bca46b830d7215265513fad043
Juli <julietabobadilla123@gmail.com>
2024-12-03 15:03:03 -0300

    Task 2 - Floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import math
 
+
 def floor(n: float) -> int:
     """
     This function takes one argument of type float and
-    return the float number rounded down to the largest 
+    return the float number rounded down to the largest
     integer less than or equal to the value n
 
     Args:
