da4fde70fc186b9a6ad46a5e4a5963da5b440f6e
Juli <julietabobadilla123@gmail.com>
2024-12-03 14:32:33 -0300

    Task 0 - Add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
+    """
+    This function takes two arguments of type float
+    and return their sum
+
+    Args:
+    a: float
+    b: float
+    """
     return a + b
