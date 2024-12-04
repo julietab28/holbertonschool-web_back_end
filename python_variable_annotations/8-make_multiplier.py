d0ec76949c118d4b68506eeb8c4a93b0ab22f6e8
Juli <julietabobadilla123@gmail.com>
2024-12-04 15:13:52 -0300

    Task 8 - Multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    This function takes one argument of type float and
+    returns a function
+
+    Args:
+        multiplier: float
+    """
+    def mult(value: float) -> float:
+        """
+        This function takes a value of type float and returns
+        a multiplication of value and multiplier
+
+        Args:
+            value: float
+        """
+        return value * multiplier
+
+    return mult
---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import List, Iterable, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    This function takes a list(an Iterable )that contains Sequences(str, list, tuple, etc)
+    and retruns a list that contains tuples that contains sequences and int
+
+    Args:
+        lst: Iterable[Sequence]
+    """
+    return [(i, len(i)) for i in lst]
