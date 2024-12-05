68021ac79eec7ad4da0f3882b3c29a162c538e0d
Juli <julietabobadilla123@gmail.com>
2024-12-05 15:46:19 -0300

    Task 1 - Concurrent coroutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module that delays
+"""
+import importlib,asyncio
+wait_random = importlib.import_module('0-basic_async_syntax').wait_random
+from typing import List
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    list_delay = []
+    list_tareas = []
+    for i in range(n):
+        tarea = asyncio.create_task(wait_random(max_delay))
+        list_tareas.append(tarea)
+
+    for tarea in asyncio.as_completed(list_tareas):
+        time = await tarea
+        list_delay.append(time)
+
+    return list_delay
