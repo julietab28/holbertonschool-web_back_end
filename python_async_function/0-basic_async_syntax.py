829fcab8a6dc1972580400538838c7c81380618a
Juli <julietabobadilla123@gmail.com>
2024-12-05 13:32:29 -0300

    Task 0 - max delay

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,10 +7,10 @@ import asyncio, random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    THis function takes one argument and returns 
+    THis function takes one argument and returns
     a random number between 0 and 10
 
-    Args: 
+    Args:
         max_delay: int
     """
     delay = random.uniform(0, max_delay)
