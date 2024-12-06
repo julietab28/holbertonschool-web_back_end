82901c39fa971a1620fb0f08f0c24570b820b528
Juli <julietabobadilla123@gmail.com>
2024-12-06 15:59:41 -0300

    Task 4 - Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Module that delays
+"""
+import asyncio
+import importlib
+from typing import List
+task_wait_random = importlib.import_module('0-basic_async_syntax').wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This function takes two arguments and returns a List
+    that contains n floats and are ordered asc
+
+    Args:
+        n: int
+        max_delay: int
+    """
+    list_delay = []
+    list_tareas = []
+    for i in range(n):
+        tarea = asyncio.create_task(task_wait_random(max_delay))
+        list_tareas.append(tarea)
+
+    for tarea in asyncio.as_completed(list_tareas):
+        time = await tarea
+        list_delay.append(time)
+
+    return list_delay
