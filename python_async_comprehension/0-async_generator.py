a089bca0850ac7f9e1de8e482d86b9de817b8f6b
Juli <julietabobadilla123@gmail.com>
2024-12-13 13:47:30 -0300

    Task 0 - Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,8 +10,6 @@ async def async_generator():
     Function that returns a list of random numbers
     between 0 and 10
     """
-    list = []
     for i in range(10):
         await asyncio.sleep(1)
-        list.append(random.randint(0, 10))
-    return list
+        yield random.randint(0, 10)
