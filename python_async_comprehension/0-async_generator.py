550420ea3412bf278cd4bce385c7e9c6e5923dd4
Juli <julietabobadilla123@gmail.com>
2024-12-13 16:10:07 -0300

    Task 0 - Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Function that returns a list of random numbers
     between 0 and 10
