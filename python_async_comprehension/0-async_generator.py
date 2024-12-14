3bf7c0b5c8eb241899dab0135f1f4ce3f95481f0
juli <julietabobadilla123@gmail.com>
2024-12-14 13:08:07 -0300

    Task 0 - Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,15 +4,14 @@ Module of the function
 """
 import asyncio
 import random
-import time
-from typing import Generator
+from typing import AsyncGenerator
 
 
-def async_generator() -> Generator[float, NoneType, NoneType]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Function that returns a list of random numbers
     between 0 and 10
     """
     for i in range(10):
-        time.sleep(1)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
