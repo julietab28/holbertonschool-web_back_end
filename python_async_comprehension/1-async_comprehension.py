18dd07d4320e28cb34478ed198d03fe173322df6
juli <julietabobadilla123@gmail.com>
2024-12-14 12:27:39 -0300

    Task 1 - Async Comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """
+Module of the function
 """
 import importlib
 import asyncio
@@ -9,4 +10,7 @@ async_generator = importlib.import_module('0-async_generator').async_generator
 
 
 async def async_comprehension():
+    """
+    Async comprehension
+    """
     return [i async for i in async_generator()]
