65d72ae86815c47dabcdd866afa7fd47d8443fbb
juli <julietabobadilla123@gmail.com>
2024-12-14 13:02:08 -0300

    Task 2 - Measure runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import importlib
 import time
 async_comprehension = importlib.import_module('1-async_comprehension').async_comprehension
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Function tahat returns the total time to complete
     all the tasks
