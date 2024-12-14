2e90ccdc35f0c21d73879bbecb1245583b043116
juli <julietabobadilla123@gmail.com>
2024-12-14 13:00:14 -0300

    Task 2 - Measure runtime

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module of the function
+"""
+import asyncio
+import importlib
+import time
+async_comprehension = importlib.import_module('1-async_comprehension').async_comprehension
+
+async def measure_runtime():
+    """
+    Function tahat returns the total time to complete
+    all the tasks
+    """
+    start = time.perf_counter()
+    task1 = asyncio.create_task(async_comprehension())
+    task2 = asyncio.create_task(async_comprehension())
+    task3 = asyncio.create_task(async_comprehension())
+    task4 = asyncio.create_task(async_comprehension())
+    await asyncio.gather(task1, task2, task3, task4)
+    end = time.perf_counter()
+
+    total_time = end - start
+
+    return total_time
