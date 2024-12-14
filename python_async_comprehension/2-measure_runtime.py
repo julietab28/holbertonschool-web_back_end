495f1bf48992e4a8f90b7ada2455d2319ed8cd1c
juli <julietabobadilla123@gmail.com>
2024-12-14 13:14:18 -0300

    Task 2 - Measure runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -5,7 +5,8 @@ Module of the function
 import asyncio
 import importlib
 import time
-async_comprehension = importlib.import_module('1-async_comprehension').async_comprehension
+comp = importlib.import_module('1-async_comprehension').async_comprehension
+
 
 async def measure_runtime() -> float:
     """
@@ -13,10 +14,10 @@ async def measure_runtime() -> float:
     all the tasks
     """
     start = time.perf_counter()
-    task1 = asyncio.create_task(async_comprehension())
-    task2 = asyncio.create_task(async_comprehension())
-    task3 = asyncio.create_task(async_comprehension())
-    task4 = asyncio.create_task(async_comprehension())
+    task1 = asyncio.create_task(comp())
+    task2 = asyncio.create_task(comp())
+    task3 = asyncio.create_task(comp())
+    task4 = asyncio.create_task(comp())
     tasks = (task1, task2, task3, task4)
     await asyncio.gather(*tasks)
     end = time.perf_counter()
