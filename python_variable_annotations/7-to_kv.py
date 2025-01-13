43b6ab5265b38f019f6fb93feb07a6396420a9d6
juli <julietabobadilla123@gmail.com>
2025-01-13 18:17:55 -0300

    Task 7 - To kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ This module creates a function to_kv
 from typing import Union, Tuple
 
 
-def to_kv(k:str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Takes two arguments and returns a tuple with
     the arguments as elements of it
