265dfd7a46122043ac09659371a76f0a6b981641
juli <julietabobadilla123@gmail.com>
2025-01-26 22:59:51 -0300

    Task 1 - Stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write(`Welcome to Holberton School, what is your name?\n`);
 
 process.stdin.on("data", (input) => {
   const name = input.toString("utf8").trim();
-  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\r`);
   process.stdout.write(`This important software is now closing\n`);
   process.exit(0);
   });
