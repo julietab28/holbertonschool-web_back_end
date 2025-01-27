391a547d3b815dc1a80935ab773a41b7a07cdc1b
juli <julietabobadilla123@gmail.com>
2025-01-27 16:48:38 -0300

    Task 4 - Http server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
-const http = require("http");
+const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('Hello Holberton School!');
 });
 
