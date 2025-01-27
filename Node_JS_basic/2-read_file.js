068d35d053ebefac9c2540ef6f2f85bf51c3b864
juli <julietabobadilla123@gmail.com>
2025-01-27 00:17:26 -0300

    Task 2 - Read file

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,25 +3,19 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-
     const lines = data.split('\n').filter((line) => line.trim() !== '');
-
     const headers = lines[0].split(',');
-
     const studentsByField = {};
 
     for (let i = 1; i < lines.length; i += 1) {
       const fields = lines[i].split(',');
-
       const field = fields[headers.indexOf('field')];
       const firstName = fields[headers.indexOf('firstname')];
 
-      if (field && firstName){
-
+      if (field && firstName) {
         if (!studentsByField[field]) {
           studentsByField[field] = [];
         }
-
         studentsByField[field].push(firstName);
       }
     }
