ea36cec9e7ac8c90f9b3bee6f6b6f2c07038d875
juli <julietabobadilla123@gmail.com>
2025-03-07 20:20:44 -0300

    Task 0 - Simple Helper Function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 """
+Pagination project
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    pass
+    """
+    Function that returns a index range ofpages
+    """
+    page_i = (page - 1) * 15
+    page_f = page + page_size
+    return tuple(page_i, page_f)
     
